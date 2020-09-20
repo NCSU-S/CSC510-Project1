ee4df506b86cc295303acc05cc03066b2a65dd6c
Hongli Wang <1023836798@qq.com>
2020-09-20 18:08:19 -0400

    Refine css and add login page

--- frontend/src/pages/JobsPage/JobsPage.tsx
+++ frontend/src/pages/JobsPage/JobsPage.tsx
@@ -4,6 +4,7 @@ import '../index.css'
 export function JobsPage(){
     return(
         <div className="jobs-page-main">
+            <div className="job-list">Job List</div>
             <Columns className="jobs-page-colomn-wrap">
                 <Columns.Column spreadFullHD={12}>
                     <Card className="job-card"title="Job1">
@@ -21,6 +22,20 @@ export function JobsPage(){
                 </Columns.Column>
                 <Columns.Column spreadFullHD={12}>
                     <Card className="job-card" title="Job3">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse diam ipsum, cursus id placerat congue, ultrices
+            eget lectus. Duis posuere, lacus sed tristique commodo, sapien turpis mollis nunc, vestibulum consectetur lectus
+            augue sit amet justo.
+                    </Card>
+                </Columns.Column>
+                <Columns.Column spreadFullHD={12}>
+                    <Card className="job-card"title="Job4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse diam ipsum, cursus id placerat congue, ultrices
+            eget lectus. Duis posuere, lacus sed tristique commodo, sapien turpis mollis nunc, vestibulum consectetur lectus
+            augue sit amet justo.
+                    </Card>
+                </Columns.Column>
+                <Columns.Column spreadFullHD={12}>
+                    <Card className="job-card"title="Job4">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse diam ipsum, cursus id placerat congue, ultrices
             eget lectus. Duis posuere, lacus sed tristique commodo, sapien turpis mollis nunc, vestibulum consectetur lectus
             augue sit amet justo.
--- frontend/src/pages/LandingPage/index.tsx
+++ frontend/src/pages/LandingPage/index.tsx
@@ -6,6 +6,36 @@ export function LandingPage() {
         <div className="landing-page-main">
             <div className="title">Find your careers here!</div>
             <p className="subtitle">We are here offering the newest jobs for you.</p>
+            <Columns className="landing-page-colomn-wrap">
+                <Columns.Column spreadFullHD={12}>
+                    <Card className="job-card"title="Job1">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse diam ipsum, cursus id placerat congue, ultrices
+            eget lectus. Duis posuere, lacus sed tristique commodo, sapien turpis mollis nunc, vestibulum consectetur lectus
+            augue sit amet justo.
+                    </Card>
+                </Columns.Column>
+                <Columns.Column spreadFullHD={12}>
+                    <Card className="job-card"title="Job2">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse diam ipsum, cursus id placerat congue, ultrices
+            eget lectus. Duis posuere, lacus sed tristique commodo, sapien turpis mollis nunc, vestibulum consectetur lectus
+            augue sit amet justo.
+                    </Card>
+                </Columns.Column>
+                <Columns.Column spreadFullHD={12}>
+                    <Card className="job-card" title="Job3">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse diam ipsum, cursus id placerat congue, ultrices
+            eget lectus. Duis posuere, lacus sed tristique commodo, sapien turpis mollis nunc, vestibulum consectetur lectus
+            augue sit amet justo.
+                    </Card>
+                </Columns.Column>
+                <Columns.Column spreadFullHD={12}>
+                    <Card className="job-card"title="Job4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse diam ipsum, cursus id placerat congue, ultrices
+            eget lectus. Duis posuere, lacus sed tristique commodo, sapien turpis mollis nunc, vestibulum consectetur lectus
+            augue sit amet justo.
+                    </Card>
+                </Columns.Column>
+            </Columns>
         </div>
     );
 }
